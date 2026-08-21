7431bd6d31bfb1dabbd35f470a4d746d19292bcb
Sacsitha <sacsitha@gmail.com>
2026-08-21 15:34:13 +0530

    Update db.php

--- backend/db/db.php
+++ backend/db/db.php
@@ -1,8 +1,13 @@
+
 <?php
-$host = "localhost";
-$username = "245091G"; // The custom username you created
-$password = "Sacsi_7453"; // The custom password you set
-$dbname = "blog_db";
-// Create connection
+$host = "sql204.infinityfree.com";
+$username = "if0_42711873";
+$password = "Sacsi7453";
+$dbname = "if0_42711873_blog_db";
+
 $conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
 ?>
